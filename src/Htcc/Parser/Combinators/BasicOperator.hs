5cba83c3c1fd279a6f5fdec74211e97cd3d0ee73
Hiroki Gomi <gomi_hiroki@cyberagent.co.jp>
2026-04-17 21:52:31 +0900

    Resolve final CodeFactor warning

--- src/Htcc/Parser/Combinators/BasicOperator.hs
+++ src/Htcc/Parser/Combinators/BasicOperator.hs
@@ -36,7 +36,7 @@ binaryOperator ::
     -> Parser i (ATree i)
 binaryOperator p opndMs = do
     m <- p
-    ($ m) . fix $ \f nd ->
+    flip fix m $ \f nd ->
         M.option nd $ choice [M.try (opM >> p) >>= ndM nd >>= f | (opM, ndM) <- opndMs]
 
 binOpBool, binOpCon :: (Monad m, Ord i, Bits i, Show i)
